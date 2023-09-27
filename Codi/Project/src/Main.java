d13df2e209551d40aa645ab8b14c8b229d0d218f
userb3rt <userb3rt@github.com>
2023-09-27 22:45:54 +0200

    PR123

--- Codi/Project/src/Main.java
+++ Codi/Project/src/Main.java
@@ -11,7 +11,9 @@ public class Main {
       String menu = "Escull una opció:";
       menu = menu + "\n 0) PR120ReadFile";
       menu = menu + "\n 1) PR121Files";
-      menu = menu + "\n 2) PR122Files";
+      menu = menu + "\n 2) PR122cat";
+      menu = menu + "\n 3) PR123sobreescriu";
+      menu = menu + "\n 4) PR123append";
       // Adapta aquí les altres classes de l’exercici (PR122cat…)
       menu = menu + "\n 100) Sortir";
       System.out.println(menu);
@@ -22,6 +24,8 @@ public class Main {
           case 0: PR120ReadFile.main(args); break;
           case 1: PR121Files.main(args); break;
           case 2: PR122cat.main(args); break;
+          case 3: PR123sobreescriu.main(args); break;
+          case 4: PR123append.main(args); break;
           // Adapta aquí les altres classes de l’exercici (PR122cat…)
           case 100: running = false; break;
           default: break;
---
+++ Codi/Project/src/PR123sobreescriu.java
@@ -0,0 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PR123sobreescriu {
+    public static void main(String[] args) {
+        try {
+            //Crea el archivo en la ruta de myFiles
+            File fraseMatrix = new File("Codi\\Project\\src\\myFiles\\frasesMatrix.txt");
+            //Habilitamos la escritura en nuestro archivo
+            FileWriter escritor = new FileWriter(fraseMatrix, false);
+            //Con este objeto tiene un metodo que nos permitira escribir dentro de nuestro archivo.
+            BufferedWriter bufferEscritura = new BufferedWriter(escritor);
+            //Le decimos lo que queremos escribir.
+            bufferEscritura.write("Yo sólo puedo mostrarte la puerta\nTú eres quien la tiene que atravesar.\n");
+            //Cerramos BufferedWriter y FileWriter
+            bufferEscritura.close();
+            escritor.close();
+            System.out.println("Archivo sobrescrito con éxito.");
+        } catch (Exception e) {
+            //Si hay un error nos lo mostrará:
+            System.out.println("Error al sobrescribir el archivo: " + e.getMessage());
+        }
+        
+        
+    }
+}
