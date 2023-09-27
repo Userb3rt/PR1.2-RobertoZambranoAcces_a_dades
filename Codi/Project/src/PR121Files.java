8017c973573d7499357c3e5480637500994aed3f
userb3rt <userb3rt@github.com>
2023-09-27 15:51:27 +0200

    exercici1 desemvolupament

--- Codi/Project/src/PR121Files.java
+++ Codi/Project/src/PR121Files.java
@@ -1,8 +1,40 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class PR121Files {
     public static void main(String[] args) {
-        File ficha = new File("MyFiles");
+        //Creació de la carpeta myFiles
+         
+        File ficha = new File("D:\\rzajr\\Documents\\PR1.2-RobertoZambranoAcces_a_dades\\Codi\\Project\\src\\myFiles");
         ficha.mkdir();
+
+        //Instanciem els 2 objectes Files 1 y 2 amb el pathname de la ruta on volem aquests arxius.
+        File file1 = new File("D:\\rzajr\\Documents\\PR1.2-RobertoZambranoAcces_a_dades\\Codi\\Project\\src\\myFiles\\file1.txt");
+        File file2 = new File("D:\\rzajr\\Documents\\PR1.2-RobertoZambranoAcces_a_dades\\Codi\\Project\\src\\myFiles\\file2.txt");
+        try {
+            //Creació dels 2 files.
+            if (!file1.exists()) {file1.createNewFile();}
+            if (!file2.exists()) {file2.createNewFile();}
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        //Rename del archiu file2.txt
+        file2.renameTo(new File("Codi\\Project\\src\\myFiles\\renamedFile.txt"));
+        
+
+        //llistat dels arxius dins de la carpeta “myFiles”
+        System.out.println("Els arxius de la carpeta són:");
+        for (File ficheroEntrada : ficha.listFiles()) {
+            System.out.println(ficheroEntrada.getName());
+        }
+
+        //Eliminació del "file1.txt"
+        
     }
 }
