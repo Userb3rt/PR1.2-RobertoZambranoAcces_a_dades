8516bdf47e4529c71152d3299c5d9126599579d9
userb3rt <userb3rt@github.com>
2023-10-11 08:49:01 +0200

    .

--- Codi/Project/src/PR132main.java
+++ Codi/Project/src/PR132main.java
@@ -7,17 +7,21 @@ import java.util.ArrayList;
 
 public class PR132main {
     public static void main(String[] args) {
+        //Instanciamos los objetos de personas
         PR132persona persona1 = new PR132persona("Maria","Lopez",36);
         PR132persona persona2 = new PR132persona("Gustavo","Ponts",63);
         PR132persona persona3 = new PR132persona("Irene","Sales",54);
-        String path = "D:\\Roberto\\Documents\\AMS2-MP06 Accés a dades 23-24\\PR1.2-RobertoZambrano\\PR1.2-RobertoZambranoAcces_a_dades\\Codi\\Project\\src\\myFiles\\PR132people.dat";
+        //Declaramos la variable string donde se encontrará nuestro archivo
+        String path = "Codi/Project/src/myFiles/PR132people.dat";
         try {
-            
+            //abrimos un bloque try ya que el FileOutPutStream lo requiere:
             FileOutputStream fos = new FileOutputStream(path);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
+            //Añadimos los objetos dentro del archivo.
             oos.writeObject(persona1);
             oos.writeObject(persona2);
             oos.writeObject(persona3);
+            //cerramos los archivos
             oos.close();
             fos.close();
         } catch (Exception e){
@@ -25,13 +29,16 @@ public class PR132main {
         }
 
         try {
+            //realizamos el importe de datos
             FileInputStream fis = new FileInputStream(path);
             ObjectInputStream ois = new ObjectInputStream(fis);
             while (true) {
+                //printeamos todos los objetos
                 PR132persona leido = (PR132persona) ois.readObject();
                 System.out.println(leido.toString());
             }
         }catch(java.io.EOFException e){
+            //como está dentro de un bucle infinito nos dará un error de este tipo que nos permitirá parar del programa.
             System.out.println("\nExportado con exito!\n ");
         } catch (Exception e) {
             // TODO: handle exception
--- Codi/Project/src/PR133mainTreballadors.java
+++ Codi/Project/src/PR133mainTreballadors.java
@@ -1,76 +1,11 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.util.List;
-
 public class PR133mainTreballadors {
-
-    static Treballador t1 = new Treballador(123,"Nicolás","Rana",2,1000.00);
-    static Treballador t2 = new Treballador(435,"Xavi","Gil",2,1800.50);
-    static Treballador t3 = new Treballador(876,"Daniel","Ramos",6,700.30);
-    static Treballador t4 = new Treballador(285,"Pedro","Drake",4,2500.00);
-    static Treballador t5 = new Treballador(224,"Joan","Potter",6,1000.00);
     
-
     public static void main(String[] args) {
-        creaciontreballadors();
         editarvalor("nom");
 
-
     }
-
-    public static void creaciontreballadors() throws IOException{
-        try {
-            FileOutputStream fos = new FileOutputStream("/myFiles/PR133treballadors.csv");
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(t1);
-            oos.writeObject(t2);
-            oos.writeObject(t3);
-            oos.writeObject(t4);
-            oos.writeObject(t5);
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            return ;
-        }
-        
-        
-    } 
 
     public static void editarvalor(String nombreatributo){
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("/myFiles/PR133treballadors.csv"))) {
-            List<Treballador> listaObjetos = (List<Treballador>) ois.readObject();
-            System.out.println(listaObjetos);
-        }catch(Exception e){
-            e.printStackTrace();
-        }   
-
-}
-
-class Treballador{
-
-    private int id;
-    private String nom;
-    private String cognom;
-    private int departament;
-    private double salari;
-
-    public Treballador(int id, String nom, String cognom,int departament,double salari){
-        this.id = id;
-        this.nom = nom;
-        this.cognom = cognom;
-        this.departament = departament;
-        this.salari = salari;
+        
     }
-    
-    @Override
-    public String toString() {
-        return id+"         "+nom+"         "+cognom+"          "+departament+"         "+salari;
-    }
-
-
-
 }
